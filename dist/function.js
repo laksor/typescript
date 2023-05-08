c88dd902b11f451c12af23f88983c63a141a69fc
laksor <al.moksud@gmail.com>
2023-05-07 23:38:04 -0400

    spread

--- dist/function.js
+++ dist/function.js
@@ -4,3 +4,12 @@ function sum(num1, num2) {
     return num1 + num2;
 }
 sum(2, 4);
+// arrow
+const addArrow = (num1, num2) => num1 + num2;
+const arr = [1, 2, 3, 4];
+const newArr = arr.map((element) => element * element);
+//spread operator
+const myFriends = ["chandler", "joey", "ross"];
+const newFriends = ["monica", "rachel", "phebe"];
+myFriends.push(...newFriends);
+console.log(myFriends);
--- src/function.ts
+++ src/function.ts
@@ -9,4 +9,11 @@ sum(2,4);
 const addArrow = (num1:number, num2:number):number => num1 + num2;
 
 const arr = [1, 2, 3, 4];
-const newArr = arr.map((elmnt:number ) => elmnt * elmnt);
+const newArr = arr.map((element:number ) => element * element);
+
+//spread operator
+
+const myFriends = ["chandler", "joey", "ross"];  
+const newFriends = ["monica", "rachel", "phebe"];
+myFriends.push(...newFriends);
+console.log(myFriends);
