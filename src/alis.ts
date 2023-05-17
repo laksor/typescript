e77484de270f91b74af2a72140efdace5f0bcc4c
laksor <al.moksud@gmail.com>
2023-05-16 23:59:21 -0400

    alias type

---
+++ src/alis.ts
@@ -0,0 +1,23 @@
+type CrushType = {
+    name: string;
+    age?: number;
+    profession: string;
+    address: string;
+}
+
+const crush1: CrushType = 
+{
+    name: "john",
+    age: 20,
+    profession: "doctor",
+    address: "uganda",
+};
+
+const crush2: CrushType = 
+{
+    name: "john mala",
+    age: 21,
+    profession: "eng",
+    address: "ban",
+};
+
